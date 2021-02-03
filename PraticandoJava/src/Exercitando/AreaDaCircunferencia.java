120d250ebb0cc7f0bb5d7195bfbcdaf56dbc6ab9
Carlos Antonio <cgomessilva2015@gmail.com>
2021-02-03 04:21:55 -0300

    Area da circunferencia

--- PraticandoJava/src/Exercitando/AreaDaCircunferencia.java
+++ PraticandoJava/src/Exercitando/AreaDaCircunferencia.java
@@ -1,5 +1,18 @@
 package Exercitando;
 
 public class AreaDaCircunferencia {
+	public static void main(String[] args) {
+		double raio = 3.4;
+		final double PI = 3.14159;
+		double area = PI * raio * raio;
+
+		System.out.println(area);
+
+//alterando valor da variável e adicionando texto
+		raio = 10;
+		area = PI * raio * raio;
+
+		System.out.println("Area = " + area + "m2.");
+	}
 
 }
