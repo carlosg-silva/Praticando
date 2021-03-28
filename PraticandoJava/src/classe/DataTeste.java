cb2368e45bf41651340c73d23b8c15f965a1b663
Carlos Antonio <cgomessilva2015@gmail.com>
2021-03-28 18:48:13 -0300

    Trabalhando com construtores

--- PraticandoJava/src/classe/DataTeste.java
+++ PraticandoJava/src/classe/DataTeste.java
@@ -6,17 +6,13 @@ public class DataTeste {
 		
 		 Data d1 = new Data();
 		 
-		 d1.dia = 26;
-		 d1.mes = 3;
-		 d1.ano = 2021;
+
 		 
 		 System.out.println(d1.obterDataFormatada());
 		 
-		 var d2 = new Data();
+		 var d2 = new Data(28, 3, 2021);
 		 
-		 d2.dia = 12;
-		 d2.mes = 06;
-		 d2.ano = 2020;
+
 		 
 		 System.out.println(d2.obterDataFormatada());
 
